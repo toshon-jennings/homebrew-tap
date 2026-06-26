61f559952ce4d77e6d55f7b1cccee1ec1701838f
Toshon <toshon.tech@gmail.com>
2026-06-26 10:02:57 -0400

    cleanmac 1.1.0: power-user flags, space savings summary, custom paths

--- Formula/cleanmac.rb
+++ Formula/cleanmac.rb
@@ -1,11 +1,16 @@
 class Cleanmac < Formula
   desc "Lightweight developer cache cleanup utility for macOS"
-  homepage "https://gist.github.com/toshon-jennings/2bf66e58aaee13c9e7a76640f70aff6e"
-  url "https://gist.githubusercontent.com/toshon-jennings/2bf66e58aaee13c9e7a76640f70aff6e/raw/cleanmac"
-  version "1.0.0"
-  sha256 "3ae52b365f883448b109512f629ff8c4450e93cddfdbe7916cc7054bc00eebde"
+  homepage "https://github.com/toshon-jennings/cleanmac"
+  url "https://github.com/toshon-jennings/cleanmac/archive/refs/tags/v1.1.0.tar.gz"
+  version "1.1.0"
+  sha256 "3be53ffe2c11de5e67509ddb6630afe5864350b8ff53f7e6ffc968d1549ea5a8"
+  license "MIT"
 
   def install
     bin.install "cleanmac"
+  end
+
+  test do
+    system "#{bin}/cleanmac", "--help"
   end
 end
