b52251d3a51206404a986139e80ee0441ddcbfeb
Toshon <toshon.tech@gmail.com>
2026-06-28 12:36:29 -0400

    cleanmac 1.1.0: update sha256

--- Formula/cleanmac.rb
+++ Formula/cleanmac.rb
@@ -3,7 +3,7 @@ class Cleanmac < Formula
   homepage "https://github.com/toshon-jennings/cleanmac"
   url "https://github.com/toshon-jennings/cleanmac/archive/refs/tags/v1.1.0.tar.gz"
   version "1.1.0"
-  sha256 "3be53ffe2c11de5e67509ddb6630afe5864350b8ff53f7e6ffc968d1549ea5a8"
+  sha256 "2f7a10039a5991de93a40a217a3c6bbf268adf15b9b54fc65540971b9dea2ee6"
   license "MIT"
 
   def install
