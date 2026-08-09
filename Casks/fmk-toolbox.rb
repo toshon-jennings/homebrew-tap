4cb6581f40e189e55e2dc08bd0e7682d593dd67a
Toshon <toshon.tech@gmail.com>
2026-08-09 19:15:25 -0400

    Bump fmk-toolbox cask to v0.1.6

--- Casks/fmk-toolbox.rb
+++ Casks/fmk-toolbox.rb
@@ -1,6 +1,6 @@
 cask "fmk-toolbox" do
-  version "0.1.5"
-  sha256 "b6b4866b2e2ef1e422e7b06bf31c62efad88637992ee787cba9e945b50296fcc"
+  version "0.1.6"
+  sha256 "55f261a60fbe1198f8b3c72b7cddcf4d072e5b59cea61afc87b6b4ea312262a2"
 
   url "https://github.com/toshon-jennings/forget-me-knot/releases/download/v#{version}/Forget-Me-Knot.ToolBox_#{version}_aarch64.dmg"
   name "Forget-Me-Knot ToolBox"
