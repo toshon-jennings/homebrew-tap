03c8f3cfb5a8cf2ca089628bda7aa899f9b337b3
Toshon <toshon.tech@gmail.com>
2026-08-09 17:24:31 -0400

    Fix DMG download URL space

--- Casks/toolbox.rb
+++ Casks/toolbox.rb
@@ -2,7 +2,7 @@ cask "toolbox" do
   version "0.1.0"
   sha256 "cd81eee124f088d036ad687647a267355cafa65555cf4b72ddfb7e0b8e46c60e"
 
-  url "https://github.com/toshon-jennings/forget-me-knot/releases/download/v#{version}/Forget-Me-Knot%20ToolBox_#{version}_aarch64.dmg"
+  url "https://github.com/toshon-jennings/forget-me-knot/releases/download/v#{version}/Forget-Me-Knot.ToolBox_#{version}_aarch64.dmg"
   name "Forget-Me-Knot ToolBox"
   desc "Lightweight macOS menubar app for managing daily tools and links"
   homepage "https://github.com/toshon-jennings/forget-me-knot"
