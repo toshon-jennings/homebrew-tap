8bd138cdd520051f2af00e1c8d464452f20e52a2
Toshon <toshon.tech@gmail.com>
2026-08-09 17:55:42 -0400

    Bump Cask to v0.1.2

--- Casks/toolbox.rb
+++ Casks/toolbox.rb
@@ -1,6 +1,6 @@
 cask "toolbox" do
-  version "0.1.1"
-  sha256 "ad691d49e7c9b98162fd59faecf206f8a3f2f3b582b82658e6e26c4a5a7d3c20"
+  version "0.1.2"
+  sha256 "e007b64bc84e12504d7939d2e9b68cb2b9cb57c0f54a81f172760977b363bd43"
 
   url "https://github.com/toshon-jennings/forget-me-knot/releases/download/v#{version}/Forget-Me-Knot.ToolBox_#{version}_aarch64.dmg"
   name "Forget-Me-Knot ToolBox"
