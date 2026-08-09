526f58a845bb93f25a34544ceb2fc2ad15f76be8
Toshon <toshon.tech@gmail.com>
2026-08-09 16:43:06 -0400

    Update Cask for Forget-Me-Knot ToolBox

--- Casks/toolbox.rb
+++ Casks/toolbox.rb
@@ -1,13 +1,13 @@
 cask "toolbox" do
   version "0.1.0"
-  sha256 "97d4fd53c3e0afd4dd71dc0bcd480a0922389ea9f8529dad86450dd680f68a56"
+  sha256 "8510cc0f395efcf6bf24f7eebfda82618f6ed0db260795ccb44ad3320b4be1a4"
 
-  url "https://github.com/toshon-jennings/forget-me-knot/releases/download/v#{version}/ToolBox_#{version}_aarch64.dmg"
-  name "ToolBox"
+  url "https://github.com/toshon-jennings/forget-me-knot/releases/download/v#{version}/Forget-Me-Knot%20ToolBox_#{version}_aarch64.dmg"
+  name "Forget-Me-Knot ToolBox"
   desc "Lightweight macOS menubar app for managing daily tools and links"
   homepage "https://github.com/toshon-jennings/forget-me-knot"
 
-  app "ToolBox.app"
+  app "Forget-Me-Knot ToolBox.app"
 
   zap trash: [
     "~/Library/Application Support/com.toolbox.app",
