e4f418998322c972327e3e14ec47219cc465dc37
Toshon <toshon.tech@gmail.com>
2026-09-15 22:53:42 -0400

    Update cleanmac to 1.4.0

--- Formula/cleanmac.rb
+++ Formula/cleanmac.rb
@@ -1,8 +1,8 @@
 class Cleanmac < Formula
   desc "Lightweight developer cache cleanup utility for macOS"
   homepage "https://github.com/toshon-jennings/cleanmac"
-  url "https://github.com/toshon-jennings/cleanmac/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "d172f518b544633b570c80bceabb7b84689e88eefe32a0fda9f9cd569aa354f9"
+  url "https://github.com/toshon-jennings/cleanmac/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "94996e5b80a2f4f7b887590ca5ebd22554717f5b0d9a18a2f9d45cd72448a65b"
   license "MIT"
 
   head "https://github.com/toshon-jennings/cleanmac.git", branch: "main"
